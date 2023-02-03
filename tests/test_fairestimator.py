cc5aa6272a26adf2495b71d2cc6f43db91ff97ad
SjoerdCor <sjoerdcornelissen@gmail.com>
2023-02-03 22:43:23 +0100

    Add tests

--- tests/test_fairestimator.py
+++ tests/test_fairestimator.py
@@ -3,26 +3,93 @@ import sys
 import pytest
 import numpy as np
 
-from sklearn.utils.estimator_checks import check_estimator
 from sklearn.base import clone as skclone
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.datasets import load_iris
 
+sys.path.append("..\..")
 sys.path.append("..")
 from fairness import fairestimator
 import importlib
 
 importlib.reload(fairestimator)
 
-# Test default parameters
 clf = RandomForestClassifier(min_samples_leaf=10, max_depth=3, random_state=42)
+regressor = RandomForestRegressor(min_samples_leaf=10, max_depth=3, random_state=42)
+
+# TODO: add pd.DataFrame to all tests
+# TODO: write docstrings for each test
+def data():
+    X = np.array([[1, 2], [2, 4], [3, 6], [4, 8]])
+    y = np.array([1] * 4)
+    return X, y
 
 
-def test_all_columns_ignored():
-    X = np.array([[1], [2], [3], [4]])
-    y = np.array([0, 0, 1, 1])
-    ib = fairestimator.IgnoringBiasClassifier(skclone(clf), ignored_cols=[0])
+def test_determine_no_cols_to_correct():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(skclone(clf), ignored_cols=None)
+    ib.fit(X, y)
+    assert ib.ignored_cols_ == []
+
+
+def test_determine_cols_to_correct():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(skclone(clf), ignored_cols=[0, 1])
+    ib.fit(X, y)
+    assert ib.ignored_cols_ == [0, 1]
+
+
+def test_impute_fixed_value():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(
+        skclone(clf), ignored_cols=[0], impute_values=[2.5]
+    )
+    ib.fit(X, y)
+
+    result = ib._prepare_new_dataset(X)
+    expected = np.array([[2.5, 2], [2.5, 4], [2.5, 6], [2.5, 8]])
+    assert np.array_equal(result, expected)
+
+
+def test_impute_fixed_value_multiple():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(
+        skclone(clf), ignored_cols=[0, 1], impute_values=[2.5, 5]
+    )
+    ib.fit(X, y)
+
+    result = ib._prepare_new_dataset(X)
+    expected = np.array([[2.5, 5], [2.5, 5], [2.5, 5], [2.5, 5]])
+    assert np.array_equal(result, expected)
+
+
+def test_calculate_correct_mean_for_imputation():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(
+        skclone(clf), ignored_cols=[0], impute_values=None
+    )
 
     ib.fit(X, y)
-    result = ib._prepare_new_dataset(X)
-    expected = np.array([[2.5], [2.5], [2.5], [2.5]])
+    result = ib.impute_values_
+    expected = np.array([2.5], dtype=np.float64)
     assert np.array_equal(result, expected)
+
+
+def test_calculate_correct_mean_for_imputation_multiple():
+    X, y = data()
+    ib = fairestimator.IgnoringBiasClassifier(
+        skclone(clf), ignored_cols=[0, 1], impute_values=None
+    )
+
+    ib.fit(X, y)
+    result = ib.impute_values_
+    expected = np.array([2.5, 5], dtype=np.float64)
+    assert np.array_equal(result, expected)
+
+
+def test_uncorrect_predictions_regressor():
+    X, y = load_iris(return_X_y=True)
+    ir = fairestimator.IgnoringBiasRegressor(skclone(regressor), ignored_cols=None)
+    ir.fit(X, y)
+    regres = skclone(regressor).fit(X, y)
+    assert np.array_equal(ir._calculate_uncorrected_predictions(X), regres.predict(X))
